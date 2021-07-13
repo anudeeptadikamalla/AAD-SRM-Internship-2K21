9688bb5a32cb48bbd45d93622a76904b89f55ef2
Anudeep Tadikamalla <63224321+anudeeptadikamalla@users.noreply.github.com>
2021-07-14 00:15:41 +0530

    Delete app directory

--- app/src/main/java/com/example/covid19report/EndPointInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.covid19report;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface EndPointInterface {
-    @GET("dayone/country/IN")
-    Call<String> getData();
-}
